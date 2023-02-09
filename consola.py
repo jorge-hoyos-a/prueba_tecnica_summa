ffbbb15c80d4d2171e61fe34e08fc0be61fd07c2
Jorge <jorandho@gmail.com>
2023-02-09 00:21:11 -0500

    Implementación del entry point en el archivo principal

--- consola.py
+++ consola.py
@@ -3,35 +3,39 @@ from parte_1 import get_indexes
 from parte_2 import get_media
 from parte_2 import get_staircase
 
-continuar = True
+def consola_retos():
+    continuar = True
 
-while continuar:
-    os.system("cls")
-    print("""\t---------------- RETOS ----------------
+    while continuar:
+        os.system("cls")
+        print("""\t---------------- RETOS ----------------
         -------- Prueba técnica Summa ---------
         1. Devolver índices de números que sumados dan un número específico
         2. Presentar la funcionalidad 1 de los tres agentes (A, B, C)
         3. Presentar la funcionalidad 2 de los tres agentes (A, B, C)
         4. Salir
         ---------------------------------------""")
-    try:
-        ret = int(input("Ingrese el número de reto que desea evaluar: "))
-        if ret == 1:
-            continuar = get_indexes()
-        elif ret == 2:
-            x = input('Ingrese un arreglo de números reales separados por coma y sin espacios: ').split(',')
-            lista_num = [int(item) for item in x]
-            continuar = get_media(lista_num)
-        elif ret == 3:
-            n = int(input('Ingrese un número entero entre 1 y 99: '))
-            if 0 < n < 100:
-                continuar = get_staircase(n)
-            else: 
-                print('Escriba un número entre 1 y 99')
-        elif ret == 4:
-            continuar = False
-        input("Presione enter para continuar...")
-    except:
-        print("No ingresó un número, vuelva a intentar")
-        input("Presione enter para continuar...")
-        continuar = True
+        try:
+            ret = int(input("Ingrese el número de reto que desea evaluar: "))
+            if ret == 1:
+                continuar = get_indexes()
+            elif ret == 2:
+                x = input('Ingrese un arreglo de números reales separados por coma y sin espacios: ').split(',')
+                lista_num = [int(item) for item in x]
+                continuar = get_media(lista_num)
+            elif ret == 3:
+                n = int(input('Ingrese un número entero entre 1 y 99: '))
+                if 0 < n < 100:
+                    continuar = get_staircase(n)
+                else: 
+                    print('Escriba un número entre 1 y 99')
+            elif ret == 4:
+                continuar = False
+            input("Presione enter para continuar...")
+        except:
+            print("No ingresó un número, vuelva a intentar")
+            input("Presione enter para continuar...")
+            continuar = True
+            
+if __name__ == '__main__':
+    consola_retos()
